464fedf290606a88c05c2e9a264eb2278e9bdf02
unknown <khardevaidehi@gmail.com>
2023-11-01 22:41:56 +0530

    commit

---
+++ code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Redirect stdout to a file called output.txt
+    FILE *file = freopen("vaidehi.txt", "w", stdout);
+
+    if (file == NULL) {
+        // Failed to open the file
+        fprintf(stderr, "Error: Unable to open the file.\n");
+        return 1;
+    }
+
+    printf("This is an example program to demonstrate output redirection using file handling.\n");
+    printf("The program's output is redirected to a file called output.txt.\n");
+
+    // Close the file stream
+    fclose(file);
+
+    return 0;
+}
